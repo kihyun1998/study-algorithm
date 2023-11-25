816855d7bad6c46938dd13ce934924ea4c837e5e
wantSome <kihyun1998@naver.com>
2023-11-25 22:03:15 +0900

    Update 2178.py

--- 백준/2178.py
+++ 백준/2178.py
@@ -1,64 +1,47 @@
 import sys
 
-"""
-지금은 모든 노드를 검사한다.
 
-이것이 아닌 모든 경우의 수를 검사하고 최소값 출력하는 걸로 변경해야 한다.
-"""
-
-def bfs(x,y):
+def bfs(si,sj,ei,ej):
     q=[]
-    q.append([x,y])
-    cnt=0
-    maze[x][y]=0
+    q.append([si,sj])
+    ans[si][sj]=1
+    # maze[start_i][start_j]=0
 
     while q:
-        exist=False
         now = q.pop(0)
-        x = now[0]
-        y = now[1] 
+        si,sj = now
+        if si==ei and sj==ej:
+            break
         # 우
-        if x+1 < n and maze[x+1][y] == 1:
-            q.append([x+1,y])
-            maze[x+1][y]=2
-            exist=True 
+        if si+1 < n and maze[si+1][sj] == 1:
+            q.append([si+1,sj])
+            maze[si+1][sj]=2
+            ans[si+1][sj]= ans[si][sj] + 1
         # 좌
-        if x-1 >= 0 and maze[x-1][y] == 1:
-            q.append([x-1,y])
-            maze[x-1][y]=2
-            exist=True
+        if si-1 >= 0 and maze[si-1][sj] == 1:
+            q.append([si-1,sj])
+            maze[si-1][sj]=2
+            ans[si-1][sj]= ans[si][sj]+1
         # 하
-        if y+1 < m and maze[x][y+1] == 1:
-            q.append([x,y+1])
-            maze[x][y+1]=2
-            exist=True
+        if sj+1 < m and maze[si][sj+1] == 1:
+            q.append([si,sj+1])
+            maze[si][sj+1]=2
+            ans[si][sj+1]=ans[si][sj]+1
         # 상
-        if y-1 >= 0 and maze[x][y-1] == 1:
-            q.append([x,y-1])
-            maze[x][y-1]=2
-            exist=True
-        print("now is",now)
-        print("q is ",q)
-        print("====maze is====")
-        for i in maze:
-            print(i)
-        print("===============")
-        if exist:
-            cnt+=1
-        print("cnt is",cnt)
-        print("\n\n")
-    return cnt
-        
-
+        if sj-1 >= 0 and maze[si][sj-1] == 1:
+            q.append([si,sj-1])
+            maze[si][sj-1]=2
+            ans[si][sj-1]=ans[si][sj]+1
 
 n,m = map(int, sys.stdin.readline().rstrip().split())
 
-maze = [[] for i in range(n)]
+maze = [[] for _ in range(n)]
+ans = [[0 for _ in range(m)] for _ in range(n)]
 
 for i in range(n):
     x = list(map(int, list(sys.stdin.readline().rstrip())))
     maze[i] = x
 
-cnt = bfs(0,0)
+cnt = bfs(0,0,n,m)
 
-print(cnt)
+print(ans[n-1][m-1])
