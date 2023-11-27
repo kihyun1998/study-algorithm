ded537ff3b209e73c180cd10f2b8a2ba3f0b1907
wantSome <kihyun1998@naver.com>
2023-11-27 17:08:18 +0900

    Update 1003.py

--- 백준/1003.py
+++ 백준/1003.py
@@ -1,8 +1,22 @@
 import sys
 
-case_arr = []
-cases = int(sys.stdin.readline().rstrip())
+def fibonacci(n,index):
+    if n==0:
+        ans[i][0] += 1
+        return 0
+    elif n==1:
+        ans[i][1] += 1
+        return 1
+    else:
+        return fibonacci(n-1,index)+fibonacci(n-2,index)
 
-for _ in range(cases):
+
+cases = int(sys.stdin.readline().rstrip())
+ans = [[0,0] for _ in range(cases)]
+
+for i in range(cases):
     x = int(sys.stdin.readline().rstrip())
-    case_arr.append(x)
+    fibonacci(x,i)
+
+for i in ans:
+    print(i)
