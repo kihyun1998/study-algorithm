3aef04a2eaa6702f2c49829f05e3eaf1c77f8d52
wantSome <kihyun1998@naver.com>
2023-11-28 21:41:53 +0900

    Update 1463.py

--- 백준/1463.py
+++ 백준/1463.py
@@ -1,11 +1,21 @@
 import sys
 
-# 연산의 최솟값 세는 변수
-count=0
+
 n = int(sys.stdin.readline().rstrip())
 
-if n != 1:
-    
+dp = [0 for _ in range(n+1)]
 
-    
-print(count)
+for i in range(2,n+1):
+    ans=[]
+    ans.append(dp[i-1]+1)
+    if i%2==0:
+        ans.append(dp[i//2]+1)
+    if i%3==0:
+        ans.append(dp[i//3]+1)    
+    dp[i]=min(ans)
+    print("i is",i)
+    print("ans is",ans)
+    print("dp[i] is ",dp[i])
+    print("===========")
+
+print(dp[n])
