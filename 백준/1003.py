05fec186b8328dc4b7cf9c9fc63e69438ec040a8
wantSome <kihyun1998@naver.com>
2023-11-28 20:53:40 +0900

    Update 1003.py

--- 백준/1003.py
+++ 백준/1003.py
@@ -1,22 +1,22 @@
 import sys
 
-def fibonacci(n,index):
-    if n==0:
-        ans[i][0] += 1
-        return 0
-    elif n==1:
-        ans[i][1] += 1
-        return 1
-    else:
-        return fibonacci(n-1,index)+fibonacci(n-2,index)
-
-
 cases = int(sys.stdin.readline().rstrip())
-ans = [[0,0] for _ in range(cases)]
+
+ans = []
 
 for i in range(cases):
     x = int(sys.stdin.readline().rstrip())
-    fibonacci(x,i)
+    dp=[[0,0] for _ in range(x+1)]
+    dp[0][0] = 1
+    dp[0][1] = 0
+    if x > 0:
+        dp[1][0] = 0
+        dp[1][1] = 1
+        for i in range(2,x+1):
+            dp[i][0] = dp[i-1][0]+dp[i-2][0]
+            dp[i][1] = dp[i-1][1]+dp[i-2][1]
+    
+    ans.append(dp[x])
 
 for i in ans:
-    print(i)
+    print(i[0],i[1])
